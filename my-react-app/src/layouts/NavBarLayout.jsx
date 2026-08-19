d15f857bbdceb91b99704463461ebb3695bdd924
charlesv12 <charles.villacruz@gmail.com>
2026-08-19 09:42:18 +0800

    App overhaul

--- my-react-app/src/layouts/NavBarLayout.jsx
+++ my-react-app/src/layouts/NavBarLayout.jsx
@@ -1,8 +1,8 @@
-export default function NavBarLayout() {
+function NavBarLayout() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">LOGO</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -12,7 +12,7 @@ export default function NavBarLayout() {
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#">Links</a>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -26,7 +26,7 @@ export default function NavBarLayout() {
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link disabled" aria-disabled="true">About me</a>
             </li>
           </ul>
           <form className="d-flex" role="search">
