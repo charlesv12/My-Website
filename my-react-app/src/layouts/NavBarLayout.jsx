ef98e425c0658d4cd1473d53bcd54a29bb2547e7
charlesv12 <charles.villacruz@gmail.com>
2026-08-19 10:02:14 +0800

    Navbar layout adjusted

--- my-react-app/src/layouts/NavBarLayout.jsx
+++ my-react-app/src/layouts/NavBarLayout.jsx
@@ -1,9 +1,17 @@
 function NavBarLayout() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary w-100">
+      <div className="container-fluid px-4">
         <a className="navbar-brand" href="#">LOGO</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
