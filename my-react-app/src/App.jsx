5a935239aa93abb239b8c4776c7451024d7ae1c5
charlesv12 <charles.villacruz@gmail.com>
2026-08-19 10:13:25 +0800

    footer added to app jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './layouts/NavBarLayout.jsx'
+import Footer from './layouts/FooterLayout.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   )
 }
