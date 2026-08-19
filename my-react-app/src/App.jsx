97ef0b94911cd6870494a024a4ad7df93ae82aa1
charlesv12 <charles.villacruz@gmail.com>
2026-08-19 10:33:54 +0800

    app jsx updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './layouts/NavBarLayout.jsx'
-import Footer from './layouts/FooterLayout.jsx'
+import FooterLayout from './layouts/FooterLayout.jsx'
 import './App.css'
 
 function App() {
